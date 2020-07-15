49d4e6651437160b9a32f7a04f37b9e11871ade6
Landon <reasoner.landon90@gmail.com>
2020-07-15 01:14:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   #sentence_1 = []
   sentence.collect do |sentence|
    #sentence_1 << word.reverse
-    sentence.reverse
+    "#{sentence.reverse}"
   end
   #sentence3.join(" ")
 end
