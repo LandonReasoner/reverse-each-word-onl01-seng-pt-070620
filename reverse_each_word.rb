a08831617e8779dec8439fe09896106c4692b106
Landon <reasoner.landon90@gmail.com>
2020-07-15 01:19:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_1 = []
   sentence.split.collect do |word|
    sentence_1 << word.reverse
-   sentence_1.join
+   sentence_1.join(" ")
   end
   #sentence3.join(" ")
 end
