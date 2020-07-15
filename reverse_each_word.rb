37e31aa315d078a01f60b0ec0b610db461ed2b15
Landon <reasoner.landon90@gmail.com>
2020-07-15 01:11:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   #sentence_1 = []
-  sentence.collect (&:reverse)
+  sentence.collects do |word.reverse|
    #sentence_1 << word.reverse
-    sentence.reverse
+    sentence
   end
   #sentence3.join(" ")
 end
